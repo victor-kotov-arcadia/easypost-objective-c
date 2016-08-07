a2c5f08f8b935c282413e1838d11889efd02e6f1
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-08-07 16:09:45 +0700

    Update EasyPosterMobile demo project

--- EasyPosterMobile/EasyPosterMobile/ViewController.m
+++ EasyPosterMobile/EasyPosterMobile/ViewController.m
@@ -2,43 +2,53 @@
 // Created by Sinisa Drpa, 2015.
 
 #import "ViewController.h"
-#import "EZPAddress.h"
+#import "EZPClient+Address.h"
 
 @interface ViewController ()
-
+@property (strong) EZPClient *easyPostClient;
 @end
 
 @implementation ViewController
 
+- (void)awakeFromNib
+{
+    self.easyPostClient = [EZPClient defaultClient];
+    NSAssert(![self.easyPostClient.APISecretKey isEqualToString:@"YOUR_TEST_API_KEY"],
+             @"Don't forget to use your real EasyPost API key");
+    NSAssert(![self.easyPostClient.APISecretKey isEqualToString:@"YOUR_LIVE_API_KEY"],
+             @"Don't forget to use your real EasyPost API key");
+}
+
 - (void)viewDidLoad {
-   [super viewDidLoad];
-   // Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
-   [super didReceiveMemoryWarning];
-   // Dispose of any resources that can be recreated.
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)createAddress:(UIButton *)sender {
-   [EZPAddress create:[self addressParameters] completion:^(EZPAddress *address, NSError *error) {
-      if (error) {
-         NSLog(@"Error: %@", [error localizedDescription]);
-      }
-      NSLog(@"Address: %@", address);
-   }];
+    [self.easyPostClient createAddressWithParameters:[self addressParameters]
+                                          completion:^(EZPAddress *address, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", [error localizedDescription]);
+        }
+        NSLog(@"Address: %@", address);
+    }];
 }
 
 - (NSDictionary *)addressParameters {
-   NSDictionary *parameters = @{@"address[name]": @"Dr. Steve Brule",
-                                @"address[street1]": @"179 N Harbor Dr",
-                                @"address[city]": @"Redondo Beach",
-                                @"address[state]": @"CA",
-                                @"address[zip]": @"90277",
-                                @"address[country]": @"US",
-                                @"address[email]": @"dr_steve_brule@gmail.com"
-                                };
-   return parameters;
+    NSDictionary *parameters = @{@"address[name]": @"Dr. Steve Brule",
+                                 @"address[street1]": @"179 N Harbor Dr",
+                                 @"address[city]": @"Redondo Beach",
+                                 @"address[state]": @"CA",
+                                 @"address[zip]": @"90277",
+                                 @"address[country]": @"US",
+                                 @"address[email]": @"dr_steve_brule@gmail.com"
+                                 };
+    return parameters;
 }
 
 @end
